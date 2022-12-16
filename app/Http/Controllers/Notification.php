76182ce77fc23516bb1ba838a3d2deb15ba04f66
ajay9495 <ajaygg7@gmail.com>
2022-12-17 00:06:10 +0530

    Changes to be committed: 	modified:   app/Http/Controllers/Notification.php

--- app/Http/Controllers/Notification.php
+++ app/Http/Controllers/Notification.php
@@ -114,7 +114,7 @@ class Notification extends BaseController{
         
 
         // $to = $this->getDeliveryPartnersOfTheStore($store_id);
-        $to = "fSGnzR2dTGOf6dGqLOjO0T:APA91bHngVjTZ9l1YrI6KGQeAroYeEet1FgFr3mPWwDYzTJrH_5ykF0N8kYwoYqCIXNXGcV29LKM1eyvYkxOr0AOKMTqmAJMtJ-tjW0C3ffd44rv8viuu5J1EmcsF4MVuieo_p6Jn-BR";
+        $registration_ids = ["fSGnzR2dTGOf6dGqLOjO0T:APA91bHngVjTZ9l1YrI6KGQeAroYeEet1FgFr3mPWwDYzTJrH_5ykF0N8kYwoYqCIXNXGcV29LKM1eyvYkxOr0AOKMTqmAJMtJ-tjW0C3ffd44rv8viuu5J1EmcsF4MVuieo_p6Jn-BR"];
         $notificationArray = [
             "body" => "test array",
             "title" => "test array",
@@ -126,7 +126,7 @@ class Notification extends BaseController{
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $data = array('to' => $to, 'notification' => $notificationArray);
+        $data = array('to' => $registration_ids, 'notification' => $notificationArray);
         $json_data = json_encode($data);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);        
         
