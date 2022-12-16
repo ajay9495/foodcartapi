fb17d9ac3a269845c11db6dc038b3dd952457d40
ajay9495 <ajaygg7@gmail.com>
2022-12-16 23:46:30 +0530

    Changes to be committed: 	modified:   app/Http/Controllers/Notification.php

--- app/Http/Controllers/Notification.php
+++ app/Http/Controllers/Notification.php
@@ -112,7 +112,7 @@ class Notification extends BaseController{
 
     function sendDeliveryNotification($store_id){
         
-        return "test data";
+        return $this->getDeliveryPartnersOfTheStore();
 
         $to = "fSGnzR2dTGOf6dGqLOjO0T:APA91bHngVjTZ9l1YrI6KGQeAroYeEet1FgFr3mPWwDYzTJrH_5ykF0N8kYwoYqCIXNXGcV29LKM1eyvYkxOr0AOKMTqmAJMtJ-tjW0C3ffd44rv8viuu5J1EmcsF4MVuieo_p6Jn-BR";
         $notificationArray = [
@@ -147,7 +147,7 @@ class Notification extends BaseController{
 
 
 
-    function deliveryPartnersOfTheStore($store_id){
+    function getDeliveryPartnersOfTheStore($store_id){
 
         return $store_id;
     }
