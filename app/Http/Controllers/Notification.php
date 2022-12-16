a14d3047789b3a900e347c4cca9cfe2adf64d6b6
ajay9495 <ajaygg7@gmail.com>
2022-12-17 00:00:21 +0530

    Changes to be committed: 	modified:   app/Http/Controllers/Notification.php

--- app/Http/Controllers/Notification.php
+++ app/Http/Controllers/Notification.php
@@ -112,13 +112,12 @@ class Notification extends BaseController{
 
     function sendDeliveryNotification($store_id){
         
-        return $this->getDeliveryPartnersOfTheStore($store_id);
 
-        $to = "fSGnzR2dTGOf6dGqLOjO0T:APA91bHngVjTZ9l1YrI6KGQeAroYeEet1FgFr3mPWwDYzTJrH_5ykF0N8kYwoYqCIXNXGcV29LKM1eyvYkxOr0AOKMTqmAJMtJ-tjW0C3ffd44rv8viuu5J1EmcsF4MVuieo_p6Jn-BR";
+        $to = $this->getDeliveryPartnersOfTheStore($store_id);
         $notificationArray = [
-            "body" => "test body",
-            "title" => "test title",
-            "subtitle" => "subtitle"
+            "body" => "test array",
+            "title" => "test array",
+            "subtitle" => "array"
         ];
 
         $ch = curl_init();
